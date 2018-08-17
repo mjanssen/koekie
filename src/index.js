e49d51bd768467a0b343463105b14bfc5e11750a
mjanssen <dev.marnix@gmail.com>
2018-08-17 13:56:19 +0200

    Added expire in functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-function setCookie(name, value, options = { expires: 3600 }) {
-  document.cookie = `${name}=${value};expires=${new Date(Date.now() + options.expires)}`;
+function setCookie(name, value, options = { expires: 1, in: 'days' }) {
+  const expiryDate = new Date();
+  if (options.in === 'hours') expiryDate.setHours(expiryDate.getHours() + options.expires);
+  if (options.in === 'days') expiryDate.setDate(expiryDate.getDate() + options.expires);
+  if (options.in === 'months') expiryDate.setMonth(expiryDate.getMonth() + options.expires);
+  if (options.in === 'years') expiryDate.setFullYear(expiryDate.getFullYear() + options.expires);
+  document.cookie = `${name}=${value};expires=${expiryDate.toGMTString()}`;
 }
 
 function getCookie(name) {
